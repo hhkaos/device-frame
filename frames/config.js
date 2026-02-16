7785791c370ce804321abede6a44b911e20f4dc6
Raul Jimenez Ortega <rjimenez@esri.com>
2026-02-16 17:44:47 +0100

    Add frame templates configuration and update index.html to use them

---
+++ frames/config.js
@@ -0,0 +1,37 @@
+window.FRAME_TEMPLATES = [
+  {
+    id: "desktop",
+    name: "Desktop frame",
+    src: "./frames/desktop.png",
+    screen: { x: 153, y: 131, w: 4404, h: 2516 },
+    screenRadius: 0
+  },
+  {
+    id: "laptop",
+    name: "Laptop frame",
+    src: "./frames/laptop.png",
+    screen: { x: 903, y: 756, w: 4199, h: 2369 },
+    screenRadius: 0
+  },
+  {
+    id: "iphone",
+    name: "Mobile iPhone frame",
+    src: "./frames/iphone.png",
+    screen: { x: 745, y: 613, w: 1803, h: 3878 },
+    screenRadius: 36
+  },
+  {
+    id: "android",
+    name: "Mobile Android frame",
+    src: "./frames/android.png",
+    screen: { x: 38, y: 46, w: 905, h: 1803 },
+    screenRadius: 24
+  },
+  {
+    id: "ipad",
+    name: "Tablet iPad frame",
+    src: "./frames/ipad.png",
+    screen: { x: 99, y: 111, w: 2325, h: 3283 },
+    screenRadius: 10
+  }
+];
